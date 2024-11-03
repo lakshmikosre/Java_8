81bd4abdc07ffb9a37959c18ed61a315f8a716db
Lakshmi Kosre <99643702+lakshmikosre@users.noreply.github.com>
2024-11-03 08:38:29 +0530

    Update Exp3.java

--- java8_BiPredicate/Exp3.java
+++ java8_BiPredicate/Exp3.java
@@ -8,8 +8,9 @@ public class Exp3 {
 	public static void main(String[] args) {
 		 
 		 Function<String , Integer> function1 = str  -> str.length();
-		 BiFunction<String , String , Integer> function2 = (str1, str2)  ->  str1.length() + str2.length();
+		 BiFunction<String , String , Integer> function2 = (str1, str2)  ->  str1.length() + str2.length() * str1.length() - str2.length() ;
 		 // Here we can take  x and y instead of str1 and str2 
+		 // We can use any operator to perform operation on two string 
 		 
 		 System.out.println(function2.apply("Lakshmi", "Laxmi"));
 
